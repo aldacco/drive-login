ce31a8a10b31e2848997446d0a1a44866500d067
aldacco <a1dac0.02@hotmail.com>
2024-09-05 22:46:58 -0500

    feat: update UI across the application for improved user experience

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,21 +1,54 @@
-"use client"
-import { useRouter } from "next/navigation"
+import { buttonVariants } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
 
 export default function SuccessPage() {
-    const router = useRouter()
-    return (
-        <main className="flex min-h-screen flex-col items-center gap-10 p-24">
-            <h1 className="text-4xl">Success</h1>
-            <button
-                className="flex items-center justify-center h-12 p-4 bg-[#29B6F6] rounded disabled:bg-gray-200"
-                onClick={() =>
-                    router.push("/")
-                }
-            >
-                <span className="text-white">
-                    Go to Home
-                </span>
-            </button>
-        </main>
-    )
+  return (
+    <main className="flex min-h-screen flex-col items-center gap-10 p-24">
+      <Card className="w-full max-w-md bg-muted/50 shadow-xl border-none rounded-md p-6">
+        <CardHeader>
+          <CardTitle className="text-center relative">
+            <span className="text-[#353132] font-bold text-3xl tracking-tighter leading-none pr-2">
+              Activation Successful!
+            </span>
+          </CardTitle>
+          <CardDescription>
+            Your Kodi device is now successfully linked. Enjoy your streaming
+            experience without any interruptions.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-8"></CardContent>
+        <CardFooter>
+          <Link href={"/"} className={buttonVariants({ variant: "link" })}>
+            Return to Home
+          </Link>
+        </CardFooter>
+      </Card>
+    </main>
+
+    // <main className="flex min-h-screen flex-col items-center gap-10 p-24">
+    //   <Card className="bg-muted/50 shadow-xl border-none rounded-md p-6">
+    //     <CardHeader>
+    //       <CardTitle className="text-center relative">
+    //         <span className="text-[#353132] font-bold text-3xl tracking-tighter leading-none pr-2">
+    //           Success
+    //         </span>
+    //       </CardTitle>
+    //     </CardHeader>
+    //     <CardContent className="grid gap-8"></CardContent>
+    //     <CardFooter>
+    //       <Link href={"/"} className={buttonVariants({ variant: "link" })}>
+    //         Go to Home
+    //       </Link>
+    //     </CardFooter>
+    //   </Card>
+    // </main>
+  );
 }
