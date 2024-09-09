7a3d43edfc1e9b7b373b68055de6c53e15cb2386
aldacco <86637158+aldacco@users.noreply.github.com>
2024-09-09 11:35:49 -0500

    Refactor: Remove need for NEXT_PUBLIC_CLIENT_URL configuration (#12)

--- src/app/refresh/route.ts
+++ src/app/refresh/route.ts
@@ -7,12 +7,14 @@ export async function POST(request: NextRequest) {
     const body = await request.formData()
     const { refresh_token: refreshToken, provider } = formDataToJson(body)
 
+    const redirect_uri = `${request.nextUrl.origin}/callback`
+
     const { data: token } = await axios.post<Token>("https://oauth2.googleapis.com/token", {
         client_id: process.env.NEXT_PUBLIC_CLIENT_ID as string,
-        redirect_uri: `${process.env.NEXT_PUBLIC_CLIENT_URL}/callback`,
         client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET as string,
         grant_type: "refresh_token",
         refresh_token: refreshToken,
+        redirect_uri,
     })
 
     return NextResponse.json(token)
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,13 +1,25 @@
-export function getGoogleAuthUrl(pin: string, clientId?: string, clientSecret?: string) {
-    const params = new URLSearchParams()
-    params.append("client_id", clientId ?? process.env.NEXT_PUBLIC_CLIENT_ID as string)
-    params.append("redirect_uri", `${process.env.NEXT_PUBLIC_CLIENT_URL}/callback`)
-    const state = clientId ? `${pin}|${clientId}|${clientSecret}` : pin
-    params.append("state", state)
-    params.append("response_type", "code")
-    params.append("scope", "https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/drive.photos.readonly https://www.googleapis.com/auth/photoslibrary.readonly profile")
-    params.append("access_type", "offline")
-    params.append("prompt", "consent")
+export function getGoogleAuthUrl(
+  pin: string,
+  redirectUri: string,
+  clientId?: string,
+  clientSecret?: string
+) {
+  const params = new URLSearchParams();
+  const state = clientId ? `${pin}|${clientId}|${clientSecret}` : pin;
 
-    return `https://accounts.google.com/o/oauth2/v2/auth?${params.toString()}`
+  params.append(
+    "client_id",
+    clientId ?? (process.env.NEXT_PUBLIC_CLIENT_ID as string)
+  );
+  params.append("redirect_uri", redirectUri);
+  params.append("state", state);
+  params.append("response_type", "code");
+  params.append(
+    "scope",
+    "https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/drive.photos.readonly https://www.googleapis.com/auth/photoslibrary.readonly profile"
+  );
+  params.append("access_type", "offline");
+  params.append("prompt", "consent");
+
+  return `https://accounts.google.com/o/oauth2/v2/auth?${params.toString()}`;
 }
